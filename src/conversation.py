758f63d3b8ffa92849264118d56498578ca7e1ad
Kaulab <154417690+kaulabbasu@users.noreply.github.com>
2025-07-23 16:26:26 +0530

    Update conversation.py

--- src/conversation.py
+++ src/conversation.py
@@ -3,7 +3,7 @@ import pandasai as pai
 from pandasai import SmartDataframe
 from langchain_community.llms import Ollama
 
-sales_by_country = pd.DataFrame({
+data = pd.DataFrame({
     "country": ["United States", "United Kingdom", "France", "Germany", "Italy", "Spain", "Canada", "Australia", "Japan", "China"],
     "revenue": [5000, 3200, 2900, 4100, 2300, 2100, 2500, 2600, 4500, 7000]
 })
