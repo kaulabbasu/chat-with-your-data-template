c1ab1191c52d98c574c27440a43e5304fb24d4f0
Kaulab <154417690+kaulabbasu@users.noreply.github.com>
2025-07-23 16:48:53 +0530

    Update conversation.py

--- src/conversation.py
+++ src/conversation.py
@@ -1,20 +1,21 @@
 import pandas as pd
-import pandasai as pai
 from pandasai import SmartDataframe
 from langchain_community.llms import Ollama
 
-data = pai.DataFrame({
+data = pd.DataFrame({
     "country": ["United States", "United Kingdom", "France", "Germany", "Italy", "Spain", "Canada", "Australia", "Japan", "China"],
     "revenue": [5000, 3200, 2900, 4100, 2300, 2100, 2500, 2600, 4500, 7000]
 })
 
+print(data)
+
 llm = Ollama(model="mistral")
 
-pai.config.set({
-    "llm": llm
-})
+# pai.config.set({
+#     "llm": llm
+# })
 
-df = SmartDataframe(data)
+df = SmartDataframe(data, config={"llm": llm})
 
 response = df.chat('Which are the top 5 countries by revenue?')
 print(response)
