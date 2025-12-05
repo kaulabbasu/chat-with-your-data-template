d21ff7999e048fb8f35ddcfefa3227d960b9f0ec
Kaulab <154417690+kaulabbasu@users.noreply.github.com>
2025-12-05 17:00:32 +0530

    Add files via upload

---
+++ src/agent_streamlit.py
@@ -0,0 +1,52 @@
+from pandasai.llm.local_llm import LocalLLM
+import streamlit as st
+import pandas as pd
+from pandasai import Agent
+
+def is_dataframe(response):
+    return isinstance(response, pd.DataFrame)
+
+model = LocalLLM(
+    api_base="http://localhost:11434/v1",
+    model="llama3"
+)
+
+config={"llm":model}
+
+st.title("Data analysis with PandasAI")
+uploaded_file= st.file_uploader("Upload a file",type=['csv'], key='first')
+uploaded_second_file= st.file_uploader("Upload a file",type=['csv'], key='second')
+
+# response = data  # Start with the original dataframe
+if uploaded_file is not None and uploaded_second_file is not None:
+    # csv_file_path = "src/countries.csv"
+    data = pd.read_csv(uploaded_file)
+    language_data = pd.read_csv(uploaded_second_file)
+    response = data
+    response_second = language_data
+    st.write(data.head(5))
+    st.write(language_data.head(5))
+    df_agent = Agent(dfs=[data, language_data],config=config)
+    while True:
+        try:
+            #prompt = input("\nEnter your prompt (or press Ctrl + C to exit): ")
+            prompt = st.text_area("Enter your prompt: ")
+            if st.button("Generate"):          
+                if prompt:
+                    with st.spinner("Generating response..."):
+                        st.write(df_agent.chat(prompt))
+                    # print("Generating response...")
+
+                    if not is_dataframe(response) or not is_dataframe(response_second):
+                        df_agent = Agent(dfs=[data, language_data],config=config)
+
+                    response = df_agent.chat(prompt)
+                    print("Response:\n", response)
+
+                    if is_dataframe(response) and is_dataframe(response_second):
+                        df_agent = Agent(dfs=[data, language_data],config=config)
+        except KeyboardInterrupt:
+            print("\nSession terminated by the user. Goodbye!")
+            break
+        except Exception as e:
+            print("An error occurred:", e)
---
+++ src/smartdf_streamlit.py
@@ -0,0 +1,30 @@
+from pandasai.llm.local_llm import LocalLLM
+import streamlit as st
+import pandas as pd
+from pandasai import SmartDataframe, Agent
+
+def is_dataframe(response):
+    return isinstance(response, pd.DataFrame)
+
+model = LocalLLM(
+    api_base="http://localhost:11434/v1",
+    model="llama3"
+)
+
+# csv_file_path = "Titanic-Dataset.csv"
+# data = pd.read_csv(csv_file_path)
+# print("First 3 rows of the Titanic dataset:\n", data.head(3))
+st.title("Data analysis with PandasAI")
+uploaded_file= st.file_uploader("Upload a file",type=['csv'])
+
+if uploaded_file is not None:
+    data =  pd.read_csv(uploaded_file)
+    st.write(data.head(5))
+    df=SmartDataframe(data,config={"llm":model})
+    prompt = st.text_area("Enter your prompt: ")
+    if st.button("Generate"):
+        if prompt:
+            
+            with st.spinner("Generating response..."):
+                st.write(df.chat(prompt))
+
